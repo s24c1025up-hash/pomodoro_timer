b70ef5dfa0f7109bb43e6a5d66287d376a80d5b1
Ryuse Oshiba <S24C1025UP@s.chibakoudai.jp>
2026-01-15 14:24:19 +0900

    pomodoro

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'pomodoro_timer'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='shiba',
+    maintainer_email='S24C1025UP@s.chibakoudai.jp',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': ['timer = pomodoro_timer.timer_node:main',
+        ],
+    },
+)
